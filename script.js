4e0c99293dbf7e067bf80de34f20ea94092f574e
unknown <anamariazaggy@gmail.com>
2020-12-14 17:47:09 -0600

    Figure=ing out the validations

--- script.js
+++ script.js
@@ -2,16 +2,31 @@
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
-      let pilotName: String = document.querySelector("input[name=pilotName]");
-      let coPilotName: String = document.querySelector("input[name=copilotName]");
-      let fuelLevel: number = document.querySelector("input[name=fuelLevel]");
-      let cargoMass: number = document.querySelector("input[name=cargoMass]");
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let coPilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
       if (pilotName.value === "" || coPilotName.value === "" || fuelLevel.value === undefined || cargoMass.value === undefined) {
       alert("All fields are required!")
       event.preventDefault();
-      };
+      }
    });
 });
+window.addEventListener("abort", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function() {
+      let fuelLevel: Number = document.querySelector("input[name=fuelLevel]");
+      let cargoMass: Number = document.querySelector("input[name=cargoMass]");
+      if (fuelLevel < 10,000) {
+         
+      }
+   })
+})
+
+
+function newFunction(event) {
+   event.preventDefault();
+}
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
