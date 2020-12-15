1b3ab235a90eac65fc4b28e28cfda0b9de70e223
The Rebellion Queen <azaggy1@outlook.com>
2020-12-14 19:37:53 -0600

    Fixed the issue

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.addEventListener("load", function() {
       let coPilotName = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
       let cargoMass = document.querySelector("input[name=cargoMass]");
-      if (pilotName.value === "" || coPilotName.value === "" || fuelLevel.value === undefined || cargoMass.value === undefined) {
+      if (pilotName.value === "" || coPilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
       alert("All fields are required!")
       event.preventDefault();
       }
@@ -15,18 +15,14 @@ window.addEventListener("load", function() {
 window.addEventListener("abort", function() {
    let form = document.querySelector("form");
    form.addEventListener("submit", function() {
-      let fuelLevel: Number = document.querySelector("input[name=fuelLevel]");
-      let cargoMass: Number = document.querySelector("input[name=cargoMass]");
+      let fuelLevel  = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
       if (fuelLevel < 10,000) {
          
       }
    })
 })
 
-
-function newFunction(event) {
-   event.preventDefault();
-}
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
